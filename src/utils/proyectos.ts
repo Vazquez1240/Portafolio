a16a3e4b5dfd6044cceaf999317cec09daa7c61f
martin <vazquezmartin1240@gmail.com>
2024-04-27 02:50:59 -0600

    se arreglaron unos proyectos

--- src/utils/proyectos.ts
+++ src/utils/proyectos.ts
@@ -29,6 +29,7 @@ export const proyectos = [
         {
             titulo: 'Encriptador',
             descripcionCorta: "Este proyecto fue los primeros que hice como parte en mi formacion en Oracle One Next, es un encriptador y desencriptador de texto.",
+            descripcionCompleta: 'Este proyecto marcó uno de los primeros hitos en mi formación en Oracle One Next. Se trata de una aplicación diseñada para encriptar y desencriptar texto. Su funcionamiento consiste en cambiar únicamente las vocales en el texto ingresado. Por ejemplo, la "a" se transforma en "ai", la "e" en "enter", la "i" en "imes", la "o" en "ober", y la "u" en "ufat". En el menú lateral derecho, se muestra el texto encriptado junto con un botón para copiarlo. Al pegar este texto en la entrada correspondiente y seleccionar la opción de desencriptar, se restaura el texto a su estado original.',
             src : '/img/desencriptador.png',
             texto: 'encriptador',
             altura: '100%',
@@ -40,6 +41,7 @@ export const proyectos = [
         {
             titulo: 'Alura Geek',
             descripcionCorta: 'Este proyecto fue el ultimo que realize en mi formacion en Oracle One Next, donde es un ecommerce donde se pueden ver productos por categorias o se puede observar un producto en especial',
+            descripcionCompleta: 'Este proyecto representó el último hito de mi formación en Oracle One Next. Se trata de una aplicación de comercio electrónico donde los usuarios pueden explorar una variedad de productos. Al iniciar la aplicación, se muestran los productos organizados por categorías, como consolas, juguetes y artículos para el hogar. La aplicación está desarrollada con React y utiliza json-server para simular una API, permitiendo así el consumo de información sobre los productos.',
             src: '/img/alura-geek.png',
             texto: 'Alura Geek',
             altura: '100%',
@@ -51,20 +53,24 @@ export const proyectos = [
         {
             titulo: 'Clima',
             descripcionCorta: 'Esta es una app para ver el clima en tu ciudad, esta app esta conectada a una API Meteorologica, donde puedes buscar las ciudades para ver su clima y dependiendo del clima sera la imagen a mostrar.',
+            descripcionCompleta: 'Esta aplicación fue un proyecto personal en el que se empleó una API meteorológica para proporcionar información climática de diferentes ciudades. Los usuarios pueden buscar su ciudad para obtener el pronóstico del clima, y según las condiciones climáticas, se muestra una imagen y datos correspondientes. Por ejemplo, si se espera un día soleado, se mostrará una imagen de un sol brillante; si se espera nubosidad, se mostrará un sol entre nubes, y así sucesivamente, ofreciendo diversas representaciones visuales según las condiciones climáticas.',
             link: 'https://vazquez1240.github.io/clima/',
             src: 'img/clima.png',
             texto: 'Clima',
             altura: '100%',
             ancho: '100%',
-            fullstack: false
+            fullstack: false,
+            envivo: {online:true, link:'https://vazquez1240.github.io/clima/'},
         },
         {
             titulo: 'Tienda virtual',
             descripcionCorta: 'Esta es una tienda virtual que realice como proyecto final en mi formacion en Tokio School, este proyecto tiene como fin la venta de productos electronicos, como tambien el ingreso de mas productos, se puede hacer un pdf de la compra entre otras cosas.',
+            descripcionCompleta: 'Esta ',
             src: 'img/tienda-virtual.png',
             texto: 'Tienda Virtual',
             altura: '100%',
             ancho: '100%',
-            fullstack: true
+            fullstack: false,
+            envivo: {online:false, link:''},
         }
     ]
